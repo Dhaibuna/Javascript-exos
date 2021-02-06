d6438114d08fba735a0b3e0389edd697254272ad
Dhaibuna <nataschagritte@gmail.com>
2021-02-06 01:13:49 +0100

    done 03-prompt-if

--- 05-javascript/01-base/03-prompt-if/script.js
+++ 05-javascript/01-base/03-prompt-if/script.js
@@ -9,8 +9,15 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-  // your code here
+(function(){
+
+  const proposal=prompt('Do you want some cake?');
+		if (proposal==="yes") {
+			alert('Good, here you are!');
+		}
+		else {
+			alert('More cake for me then :p !');
+		}
 
 })();
