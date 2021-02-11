5bc11ffdcfa10e4ea8273096b3f64bb492b30acf
Dhaibuna <nataschagritte@gmail.com>
2021-02-11 23:37:14 +0100

    Done 4.4

--- 05-javascript/04-dates/04-locale-date/script.js
+++ 05-javascript/04-dates/04-locale-date/script.js
@@ -9,10 +9,26 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+let dateOfToday = new Date(); 
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric' , minute: 'numeric' };
+// Arguments 
+
 (function() {
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-
     // your code here
+    document.getElementById("target").innerHTML = (dateOfToday.toLocaleDateString('en-UK', options)) ;
 
 })();
+
+/* 
+Qu'est ce que j'ai ? 
+
+-L'Id de mon content que je dois changer
+
+De quoi est-ce que j'ai besoin ? 
+
+- D'aller chercher mon content pour le modifier avec InnerHTML
+- Variable qui va stocker la date à afficher ( en anglais )
+
+*/
