d8aab838d18a0faf3b78cb8e7d1b6aea349e793b
Dhaibuna <nataschagritte@gmail.com>
2021-02-12 10:21:56 +0100

    Done 5.4

--- 05-javascript/05-arrays/04-walk-two/script.js
+++ 05-javascript/05-arrays/04-walk-two/script.js
@@ -9,7 +9,7 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
     var fruits = [
         "pomme",
@@ -27,5 +27,11 @@
     ];
 
     // your code here
+
+    document.getElementById("run").addEventListener("click", function () {
+        fruits.forEach(function (item, index, array) {
+            console.log(index);
+        });
+    });
 
 })();
