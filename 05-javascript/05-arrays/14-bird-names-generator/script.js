ad921edd4e7244b2a2b4ea01ddc86ef5659f9c77
Dhaibuna <nataschagritte@gmail.com>
2021-02-16 09:54:52 +0100

    update 5.14

--- 05-javascript/05-arrays/14-bird-names-generator/script.js
+++ 05-javascript/05-arrays/14-bird-names-generator/script.js
@@ -11,18 +11,18 @@
 
 (() => {
     const birds = [
-        {name: "mouette", fem: true},
-        {name: "corbeau"},
-        {name: "mésange", fem: true},
-        {name: "hibou"},
-        {name: "buse", fem: true},
-        {name: "pigeon"},
-        {name: "pie", fem: true},
-        {name: "vautour"},
-        {name: "faucon"},
-        {name: "rouge-gorge"},
-        {name: "tourterelle", fem: true},
-        {name: "corneille", fem: true},
+        { name: "mouette", fem: true },
+        { name: "corbeau" },
+        { name: "mésange", fem: true },
+        { name: "hibou" },
+        { name: "buse", fem: true },
+        { name: "pigeon" },
+        { name: "pie", fem: true },
+        { name: "vautour" },
+        { name: "faucon" },
+        { name: "rouge-gorge" },
+        { name: "tourterelle", fem: true },
+        { name: "corneille", fem: true },
     ];
     const adjectives = new Set([
         "cendré",
@@ -39,4 +39,53 @@
     ]);
 
     // your code here
+
+    const birdsNamesGrabbing = birds.map(function (birds) {
+        return birds.name;
+    });
+    //console.log(birdsNamesGrabbing);
+
+    const birdsNamesFem = birds.map(function (birds) {
+        return birds.fem;
+    });
+    //console.log(birdsNamesFem);
+
+    const birdsAdjectives = Array.from(adjectives); // Avec Array.from, je crée un tableau apd de mon set 
+    //console.log(birdsAdjectives); 
+
+
+    document.getElementById("run").addEventListener("click", function () {
+        const randomBird = Math.floor(Math.random() * birdsNamesGrabbing.length);
+        //console.log(birdsNamesGrabbing [randomBird]);
+        const randombirdsAdjectives = Math.floor(Math.random() * birdsAdjectives.length);
+        //console.log( birdsAdjectives[randombirdsAdjectives]);
+        if (birds.fem == true) {
+            document.getElementById("target").innerHTML = `${birdsNamesGrabbing[randomBird]} ${birdsAdjectives[randombirdsAdjectives]}e`;
+        } else {
+            document.getElementById("target").innerHTML = `${birdsNamesGrabbing[randomBird]} ${birdsAdjectives[randombirdsAdjectives]}`;
+        };
+    });
 })();
+
+// Ne prends pas en compte la condition ....et donc n'accorde pas l'adj
+
+
+
+/*
+
+1. Qu'est ce que j'ai ?
+
+- Deux arrays : Noms + adjectifs à accorder
+- Un id run pour l'écoute
+- Un id target pour la phrase à changer
+
+2. De quoi j'ai besoin ?
+
+- D'une écoute sur le bouton run
+- Une manip du HTML inner.HTML pour modifier aléatoirement le text au click.
+- D'une méthode pour aller chercher de manière aléatoire un élément de chaques tableaux
+- Le problème qui se pose est d'aller chercher des clés dans les objets du premier tableau.
+- Une concaténation d'un des éléments du tableau noms et d'un des élément du tableau adjectif
+- D'un condition if pour savoir si la clé "fem=true"
+
+*/
