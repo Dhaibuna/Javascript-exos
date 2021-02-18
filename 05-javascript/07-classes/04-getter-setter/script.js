0e1fae6a1aa98b2cb5df778f6990a8637690195a
Dhaibuna <nataschagritte@gmail.com>
2021-02-18 12:02:40 +0100

    Done 7.4

--- 05-javascript/07-classes/04-getter-setter/script.js
+++ 05-javascript/07-classes/04-getter-setter/script.js
@@ -11,4 +11,27 @@
 
 (() => {
     // your code here
+    // Je crée la classe "Person" avec 2 propriétés : "firstname" & "lastname"
+    class Person{
+        constructor(firstname, lastname){
+            this.firstname = firstname; 
+            this.lastname = lastname;
+        }
+        get identity() {   // Avec get je vais rechercher.
+            return `${this.firstname} ${this.lastname}`;
+        };
+
+        set identity(separated) {  // Set est un mutateur.
+           let Newidentity = separated.split(' ');
+            this.firstname = Newidentity[0];
+            this.lastname = Newidentity[1];
+        };
+    }
+
+    document.getElementById("run").addEventListener("click", ()=>{
+        let person1 = new Person("Ulysse", "Lechat");
+        console.log(person1.identity);
+        person1.identity = ("Sushi Lechien"); // En une seule string, parce que la fonction "Setter" sépare d'elle même la string de par son contenu 
+        console.log(person1.identity);
+    });
 })();
