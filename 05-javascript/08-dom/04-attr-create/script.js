044e722120b3e43564e1cc403d8e35f2abfd772b
Dhaibuna <nataschagritte@gmail.com>
2021-02-19 15:05:53 +0100

    Done 8.4

--- 05-javascript/08-dom/04-attr-create/script.js
+++ 05-javascript/08-dom/04-attr-create/script.js
@@ -9,8 +9,19 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
     // your code here
+    // get the value
+    let image = document.getElementById("source").getAttribute("data-image");
+    //console.log(img);
+
+    //Set value to create a new image element
+    let newImg = document.createElement("img");
+    newImg.setAttribute("src", image);  // Attribut - Value
+    //console.log(newImg);
+    document.getElementById("target").appendChild(newImg);
+    //Remove attribute 
+    document.getElementById("source").removeAttribute("data-image");
 
 })();
