04348ad6245b13763c013cc109d3ca1eb177d98c
Dhaibuna <nataschagritte@gmail.com>
2021-02-19 15:53:27 +0100

    Almost Done 8.5

--- 05-javascript/08-dom/05-hover-image/script.js
+++ 05-javascript/08-dom/05-hover-image/script.js
@@ -9,8 +9,15 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
     // your code here
+    document.querySelector("img").addEventListener("mouseover", () => {
+        let hover = document.querySelector("img");
+        hover.setAttribute('src', 'data-hover');
+    })
 
 })();
+
+
+// L'image ne se display pas ... quel malheur... :(
