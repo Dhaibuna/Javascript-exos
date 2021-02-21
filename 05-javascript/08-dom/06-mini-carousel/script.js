3ad3341e9d7049963a14cba83be51df777fca12e
Dhaibuna <nataschagritte@gmail.com>
2021-02-22 00:37:39 +0100

    Done Carousel

--- 05-javascript/08-dom/06-mini-carousel/script.js
+++ 05-javascript/08-dom/06-mini-carousel/script.js
@@ -9,9 +9,9 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
 
-    var gallery= [
+    var gallery = [
         "../../_shared/img/bell.svg",
         "../../_shared/img/clock.svg",
         "../../_shared/img/compass.svg",
@@ -21,4 +21,25 @@
 
     // your code here
 
+    const getImgDom = document.querySelector("img");
+    let index = 0;
+
+    document.getElementById("next").addEventListener("click", () => {
+        getImgDom.setAttribute("src", gallery[index]);
+        
+        if (gallery.length > index) {
+            index++;  // index = index+1
+        }else{
+            index=0;
+        }
+    });
+
 })();
+
+
+/*
+
+- Aller chercher l'attribut avec Get et le stocker dans une variable
+- Modifier l'attribut avec set 
+- Condition if pour la longueur de mon tableau
+*/
