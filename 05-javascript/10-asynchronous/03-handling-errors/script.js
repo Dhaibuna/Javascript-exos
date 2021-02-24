9e76afe75a1919d2dd474834453c31801c75e8b4
Dhaibuna <nataschagritte@gmail.com>
2021-02-24 22:01:35 +0100

    Done 10.3

--- 05-javascript/10-asynchronous/03-handling-errors/script.js
+++ 05-javascript/10-asynchronous/03-handling-errors/script.js
@@ -11,4 +11,9 @@
 
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click", () =>{
+        window.lib.getPersons((err, people)=>{
+            console.log(err, people);
+        });
+    });
 })();
