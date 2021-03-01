ba83ada950fead9dfe46d906787ca58045add568
Dhaibuna <nataschagritte@gmail.com>
2021-03-01 10:10:25 +0100

    updates

--- 05-javascript/11-fetch/02-list-to-template/script.js
+++ 05-javascript/11-fetch/02-list-to-template/script.js
@@ -11,4 +11,23 @@
 
 (() => {
     // your code here
+
+    const displayInTarget = document.querySelector("#target"); // Je vais chercher l'élément dans mon HTML 
+    let heroesStructure = document.getElementById("tpl-hero");
+    document.getElementById("run").addEventListener("click", () => {
+        fetch("http://localhost:3000/heroes") // J appelle la méthode fetch 
+            .then(response => response.json()) // Je formate la réponse que je vais recevoir au format JSON
+            .then(data => {
+                console.log(data);
+                data.forEach(({ name, alterEgo, powers }) => {
+                    let iteratedNode = heroesStructure.cloneNode(true);
+                    iteratedNode.querySelector(".name").textContent = name;
+                    iteratedNode.querySelector(".alter-ego").textContent = alterEgo;
+                    iteratedNode.querySelector(".powers").textContent = powers;
+                    displayInTarget.appendChild(iteratedNode);
+                });
+                console.log(heroesStructure)
+            });
+
+    });
 })();
